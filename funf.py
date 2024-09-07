d9c2adda5efc544fedfe916a07523bc9b972d034
kiras <remixsidenation@gmail.com>
2024-09-07 19:46:15 -0300

    a

--- funf.py
+++ funf.py
@@ -6,4 +6,6 @@ init(autoreset=True)
 def funf():
     print(Fore.BLUE+'Bem vindo ao painel de funções!')
     print('.' * 45)
+    print('(1):Somar um valor + outro.')
+    print('(2)  k:')
         
